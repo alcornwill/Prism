8753dd945e4132b45c361f1280a9bd8d6e9a67c2
alcornwill <alcornwill@users.noreply.github.com>
2015-01-18 22:09:13 +0000

    Initial commit

---
+++ prism.js
@@ -0,0 +1,126 @@
+var gl = null;
+
+// 500 x 500
+var canvasWidth = 500;
+var canvasHeight = 500;
+
+function realign()
+{
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	
+	var canvas = document.getElementById("canvas");
+	canvas.style.top = ((h / 2) - (canvasWidth / 2)) + "px";
+	canvas.style.left = ((w / 2) - (canvasHeight / 2)) + "px";
+}
+
+function init() 
+{
+	realign();
+	var canvas = document.getElementById("canvas");
+	gl = canvas.getContext("experimental-webgl");
+	gl.clearColor(0.97, 0.97, 0.97, 1.0);
+	initProgram();
+	initBuffers();
+	animate();
+}
+
+var _color = null;
+var _position = null;
+
+function initProgram()
+{
+	// and initShaders
+	var getShader = function(source, type) {
+		var shader = gl.createShader(type);
+		gl.shaderSource(shader, source);
+		gl.compileShader(shader);
+		return shader;
+	};
+	
+	var vertexShader = getShader(vertexShaderSource, gl.VERTEX_SHADER);
+	
+	var fragmentShader = getShader(fragmentShaderSource, gl.FRAGMENT_SHADER);
+	
+	var shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertexShader);
+	gl.attachShader(shaderProgram, fragmentShader);
+	
+	gl.linkProgram(shaderProgram);
+	
+	_color = gl.getAttribLocation(shaderProgram, "color");
+	_position = gl.getAttribLocation(shaderProgram, "position");
+	
+	gl.enableVertexAttribArray(_color);
+	gl.enableVertexAttribArray(_position);
+	
+	gl.useProgram(shaderProgram);
+}
+
+var VBO = null;
+var IBO = null;
+
+function initBuffers()
+{
+	var triangleVertex = [
+		-1, -1,
+		0, 0, 1,
+		1, -1,
+		1, 1, 0, 
+		1, 1,
+		1, 0, 0,
+		-1, 1,
+		0, 1, 1
+	];
+	
+	var triangleFaces = [
+		0, 1, 2,
+		0, 2, 3
+	];
+	
+	VBO = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertex), gl.STATIC_DRAW);
+
+	IBO = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, IBO);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(triangleFaces), gl.STATIC_DRAW);
+
+}
+	
+function animate()
+{
+	clear(gl);
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
+	gl.vertexAttribPointer(_position, 2, gl.FLOAT, false, 4*(2+3), 0);
+	gl.vertexAttribPointer(_color, 3, gl.FLOAT, false, 4*(2+3), 2*4);
+	
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, IBO);
+	gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+	gl.flush();
+	
+	window.requestAnimationFrame(animate);
+}
+	
+window.onkeydown = checkKey;
+
+function checkKey(e)
+{
+	switch(e.keyCode)
+	{
+		case 49:{ // 1
+			// do nothing. Just being an example
+			// can I use event handlers or something?
+			// I'm going to have a lot of cases otherwise.
+		return;
+		}
+	}
+}
+
+function clear(ctx) 
+{
+	ctx.viewport(0, 0, canvasWidth, canvasHeight);
+	ctx.clear(ctx.COLOR_BUFFER_BIT);
+}
+
---
+++ shaders.js
@@ -0,0 +1,19 @@
+var vertexShaderSource = "\n\
+attribute vec2 position; //the position of the point\n\
+attribute vec3 color;  //the color of the point\n\
+\n\
+varying vec3 vColor;\n\
+void main(void) { //pre-built function\n\
+gl_Position = vec4(position, 0., 1.); //0. is the z, and 1 is w\n\
+vColor=color;\n\
+}";
+
+var fragmentShaderSource = "\n\
+precision mediump float;\n\
+\n\
+\n\
+\n\
+varying vec3 vColor;\n\
+void main(void) {\n\
+gl_FragColor = vec4(vColor, 1.);\n\
+}";
