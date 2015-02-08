926e4776c7191a3b47f6f078e84832e18302d2db
Will <alcornwill@gmail.com>
2015-02-08 00:33:21 +0000

    Things have got pretty mental.

--- prism.js
+++ prism.js
@@ -15,14 +15,14 @@ function init()
 {
 	realign();
 	constructTerminal();
-	setInterval(drawCursor(), 500);
+	setInterval(toggleCursor, 500);
+	textEditorBindings();
 }
 
 var charX = 52;
 var charY = 28;
 
 var matrix = [];
-
 
 function constructTerminal()
 {
@@ -42,6 +42,10 @@ function getLine(y)
 	var line = "";
 	for (var x=0; x<charX; x++)
 	{
+		if (cursor.x == x && cursor.y == y && cursorOn)
+		{
+			line += '\u2588';
+		}
 		line += matrix[y][x];
 	}
 	
@@ -79,6 +83,7 @@ function vec(x, y)
 }
 
 var cursor = vec(0, 0);
+var cursorOn = false;
 
 function print(ch)
 {
@@ -89,6 +94,13 @@ function print(ch)
 	}
 	matrix[cursor.y][cursor.x] = ch;
 	cursor.x++;
+	update();
+}
+
+function vPrint(ch)
+{
+	matrix[cursor.y][cursor.x] = ch;
+	cursor.y++;
 	update();
 }
 
@@ -129,9 +141,50 @@ function newLine()
 	}
 }
 
-function drawCursor()
+function fannyBlast()
 {
-	console.log("cursor");
+	printAt(20, 10, "Fanny Blast!");
+}
+
+function printAt(x, y, text)
+{
+	cursor.x = x;
+	cursor.y = y;
+	
+	for (var i=0; i<text.length; i++)
+	{
+		print(text[i]);
+	}
+}
+
+function drawLine(x, y, ch, length)
+{
+	cursor.x = x;
+	cursor.y = y;
+	
+	for (var i=0; i<length; i++)
+	{
+		print(ch);
+	}
+}
+
+function drawVLine(x, y, ch, length)
+{
+	cursor.x = x;
+	cursor.y = y;
+	
+	for (var i=0; i<length; i++)
+	{
+		vPrint(ch);
+	}
+}
+
+function toggleCursor()
+{
+	cursorOn
+	? cursorOn = false
+	: cursorOn = true;
+	update();
 }
 
 // Could do this kind of thing:
@@ -143,288 +196,391 @@ function drawCursor()
 // var pattern2 = ...;
 // var punctuation = ...;
 //
-// and then (somehow) generate these Key variables.
+// and then (somehow) generate these contexts.
 
-var aKey = {ch:'a', shift:'A', keyCode:65};
-var bKey = {ch:'b', shift:'B', keyCode:66};
-var cKey = {ch:'c', shift:'C', keyCode:67};
-var dKey = {ch:'d', shift:'D', keyCode:68};
-var eKey = {ch:'e', shift:'E', keyCode:69};
-var fKey = {ch:'f', shift:'F', keyCode:70};
-var gKey = {ch:'g', shift:'G', keyCode:71};
-var hKey = {ch:'h', shift:'H', keyCode:72};
-var iKey = {ch:'i', shift:'I', keyCode:73}; 
-var jKey = {ch:'j', shift:'J', keyCode:74}; 
-var kKey = {ch:'k', shift:'K', keyCode:75};
-var lKey = {ch:'l', shift:'L', keyCode:76};
-var mKey = {ch:'m', shift:'M', keyCode:77};
-var nKey = {ch:'n', shift:'N', keyCode:78};
-var oKey = {ch:'o', shift:'O', keyCode:79};
-var pKey = {ch:'p', shift:'P', keyCode:80};
-var qKey = {ch:'q', shift:'Q', keyCode:81};
-var rKey = {ch:'r', shift:'R', keyCode:82};
-var sKey = {ch:'s', shift:'S', keyCode:83};
-var tKey = {ch:'t', shift:'T', keyCode:84};
-var uKey = {ch:'u', shift:'U', keyCode:85};
-var vKey = {ch:'v', shift:'V', keyCode:86};
-var wKey = {ch:'w', shift:'W', keyCode:87};
-var xKey = {ch:'x', shift:'X', keyCode:88};
-var yKey = {ch:'y', shift:'Y', keyCode:89};
-var zKey = {ch:'z', shift:'Z', keyCode:90};	
+function key(normal, shift, alt, meta, shiftAlt, shiftMeta, altMeta, shiftAltMeta)
+{
+	return {
+	normal: normal,
+	shift: shift,
+	alt: alt,
+	meta: meta,
+	shiftAlt: shiftAlt,
+	shiftMeta: shiftMeta,
+	altMeta: altMeta,
+	shiftAltMeta: shiftAltMeta
+	};
+}
+
+var keys = [];
+
+function textEditorBindings()
+{
+	// Backspace
+	keys[8] = key(
+	function(){deleteChar()},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	// Enter
+	keys[13] = key(
+	function(){newLine()},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	// Space
+	keys[32] = key(
+	function(){print('\u00a0')},
+	function(){print('\u00a0')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	// A to Z
+	keys[65] = key(
+	function(){print('a')},
+	function(){print('A')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[66] = key(
+	function(){print('b')},
+	function(){print('B')},
+	function(){printAt(20, 10, "Fanny Blast!")},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[67] = key(
+	function(){print('c')},
+	function(){print('C')},
+	function(){drawLine(0, 0, '\u2500', 10)},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[68] = key(
+	function(){print('d')},
+	function(){print('D')},
+	function(){drawVLine(0, 0, '\u2502', 10)},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[69] = key(
+	function(){print('e')},
+	function(){print('E')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[70] = key(
+	function(){print('f')},
+	function(){print('F')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[71] = key(
+	function(){print('g')},
+	function(){print('G')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[72] = key(
+	function(){print('h')},
+	function(){print('H')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[73] = key(
+	function(){print('i')},
+	function(){print('I')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[74] = key(
+	function(){print('j')},
+	function(){print('J')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[75] = key(
+	function(){print('k')},
+	function(){print('K')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[76] = key(
+	function(){print('l')},
+	function(){print('L')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[77] = key(
+	function(){print('m')},
+	function(){print('M')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[78] = key(
+	function(){print('n')},
+	function(){print('N')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[79] = key(
+	function(){print('o')},
+	function(){print('O')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[80] = key(
+	function(){print('p')},
+	function(){print('P')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[81] = key(
+	function(){print('q')},
+	function(){print('Q')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[82] = key(
+	function(){print('r')},
+	function(){print('R')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[83] = key(
+	function(){print('s')},
+	function(){print('S')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[84] = key(
+	function(){print('t')},
+	function(){print('T')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[85] = key(
+	function(){print('u')},
+	function(){print('U')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[86] = key(
+	function(){print('v')},
+	function(){print('V')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[87] = key(
+	function(){print('w')},
+	function(){print('W')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[88] = key(
+	function(){print('x')},
+	function(){print('X')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[89] = key(
+	function(){print('y')},
+	function(){print('Y')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+	
+	keys[90] = key(
+	function(){print('z')},
+	function(){print('Z')},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){},
+	function(){}
+	);
+}
 
 window.onkeydown = checkKey;
 
 function checkKey(e)
 {
 	e.preventDefault();
-	switch(e.keyCode)
+	if (e.shiftKey && e.altKey && e.metaKey)
 	{
-		case aKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(aKey.shift);
-				return;
-			}
-			print(aKey.ch);		
-			return;
-		}
-		case bKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(bKey.shift);
-				return;
-			}
-			print(bKey.ch);
-			return;
-		}
-		case cKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(cKey.shift);
-				return;
-			}
-			print(cKey.ch);
-			return;
-		}
-		case dKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(dKey.shift);
-				return;
-			}
-			print(dKey.ch);
-			return;
-		}
-		case eKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(eKey.shift);
-				return;
-			}
-			print(eKey.ch);
-			return;
-		}
-		case fKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(fKey.shift);
-				return;
-			}
-			print(fKey.ch);
-			return;
-		}
-		case gKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(gKey.shift);
-				return;
-			}
-			print(gKey.ch);
-			return;
-		}
-		case hKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(hKey.shift);
-				return;
-			}
-			print(hKey.ch);
-			return;
-		}
-		case iKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(iKey.shift);
-				return;
-			}
-			print(iKey.ch);
-			return;
-		}
-		case jKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(jKey.shift);
-				return;
-			}
-			print(jKey.ch);
-			return;
-		}
-		case kKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(kKey.shift);
-				return;
-			}
-			print(kKey.ch);
-			return;
-		}
-		case lKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(lKey.shift);
-				return;
-			}
-			print(lKey.ch);
-			return;
-		}
-		case mKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(mKey.shift);
-				return;
-			}
-			print(mKey.ch);
-			return;
-		}		
-		case nKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(nKey.shift);
-				return;
-			}
-			print(nKey.ch);
-			return;
-		}
-		case oKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(oKey.shift);
-				return;
-			}
-			print(oKey.ch);
-			return;
-		}
-		case pKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(pKey.shift);
-				return;
-			}
-			print(pKey.ch);
-			return;
-		}
-		case qKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(qKey.shift);
-				return;
-			}
-			print(qKey.ch);
-			return;
-		}
-		case rKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(rKey.shift);
-				return;
-			}
-			print(rKey.ch);
-			return;
-		}
-		case sKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(sKey.shift);
-				return;
-			}
-			print(sKey.ch);
-			return;
-		}
-		case tKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(tKey.shift);
-				return;
-			}
-			print(tKey.ch);
-			return;
-		}
-		case uKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(uKey.shift);
-				return;
-			}
-			print(uKey.ch);
-			return;
-		}
-		case vKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(vKey.shift);
-				return;
-			}
-			print(vKey.ch);
-			return;
-		}
-		case wKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(wKey.shift);
-				return;
-			}
-			print(wKey.ch);
-			return;
-		}
-		case xKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(xKey.shift);
-				return;
-			}
-			print(xKey.ch);
-			return;
-		}
-		case yKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(yKey.shift);
-				return;
-			}
-			print(yKey.ch);
-			return;
-		}
-		case zKey.keyCode:{
-			if (e.shiftKey)
-			{
-				print(zKey.shift);
-				return;
-			}
-			print(zKey.ch);
-			return;
-		}
-		case 8:{ // Backspace
-			deleteChar();
-			return;
-		}
-		case 32:{ // Space
-		    print('\u00a0');
-			return;
-		}
-		case 13:{ // Enter
-			newLine();
-			return;
-		}
+		keys[e.keyCode].shiftAltMeta();
+		return;
 	}
+	if (e.shiftKey && e.altKey)
+	{
+		keys[e.keyCode].shiftAlt();
+		return;
+	}
+	if (e.shiftKey && e.metaKey)
+	{
+		keys[e.keyCode].shiftMeta();
+		return;
+	}
+	if (e.altKey && e.metaKey)
+	{
+		keys[e.keyCode].altMeta();
+		return;
+	}
+	if (e.shiftKey)
+	{
+		keys[e.keyCode].shift();
+		return;
+	}
+	if (e.altKey)
+	{
+		keys[e.keyCode].alt();
+		return;
+	}
+	if (e.metaKey)
+	{
+		keys[e.keyCode].meta();
+		return;
+	}
+	keys[e.keyCode].normal();
+	return;
 }
 
